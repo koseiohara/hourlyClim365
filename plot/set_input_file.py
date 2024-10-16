7794d1bc605b3a6fdf0a7baa55f3a3bad853ad59
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2024-10-16 21:27:21 +0900

    Change of Directory Structure

--- plot/set_input_file.py
+++
@@ -1,7 +0,0 @@
-def set_input_file(dataset, ini, fin, dim, var, flag, extension):
-    output = '../shift/output/{}/{}_{}_{}_ALL_{}_SHIFT_{}{}.{}'.format(dataset, dataset, ini, fin, dim, var, flag, extension)
-    print('  << ' + output)
-
-    return output
-
-
